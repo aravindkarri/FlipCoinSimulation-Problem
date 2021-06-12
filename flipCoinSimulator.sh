e57a37d8a9a71c4bd502b638d5eb9ba4a2e4f0e9
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 17:08:49 +0530

    code to calculate won or tie till condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,17 +5,23 @@ echo "Welcome to FlipCoinSimulator program"
 headCount=0
 tailCount=0
 
+function checkWinner()
+{
+	randomCheck=$1
+	if [ $randomCheck -eq 1 ]
+        then
+                ((headCount++))
+                echo "head is winner"
+        else
+                ((tailCount++))
+                echo "tail is winner"
+        fi
+}
+
 while [[ $headCount -ne 21 && $tailCount -ne 21 ]]
 do
-	randomCheck=$((RANDOM%2))
-	if [ $randomCheck -eq 1 ]
-	then
-		((headCount++))
-		echo "head is winner"
-	else
-		((tailCount++))
-		echo "tail is winner"
-	fi
+	 randomCheck=$((RANDOM%2))
+	 checkWinner $randomCheck
 done
 echo "head won $headCount times"
 echo "tail won $tailCount times"
