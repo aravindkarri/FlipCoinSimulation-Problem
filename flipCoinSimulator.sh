166a077fa10b60be67aac40247d7151e60f3a1ef
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 16:06:19 +0530

    Add: code to display head or tail as wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome to FlipCoinSimulator program"
+
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq 1 ]
+then
+        echo "Head is winner"
+else
+        echo "Tail is winner"
+fi
