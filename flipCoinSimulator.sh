65bc7d81e93870dc1cccd391c7e771232f3e5447
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 16:15:02 +0530

    Add: code to calculate number of times won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,10 +2,23 @@
 
 echo "Welcome to FlipCoinSimulator program"
 
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq 1 ]
-then
-        echo "Head is winner"
-else
-        echo "Tail is winner"
-fi
+headCount=0
+tailCount=0
+
+for ((i=0;i<10;i++))
+do
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq 1 ]
+	then
+		((headCount++))
+		echo "head is winner"
+	else
+		((tailCount++))
+		echo "tail is winner"
+	fi
+done
+echo "Head won $headCount times"
+echo "Tail won $tailCount times"
+
+
+
